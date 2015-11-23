01b4947c711c2d7aada8114aecca56aa85c9214d
Krzesimir Nowak <krzesimir@kinvolk.io>
2015-11-23 16:24:56 +0100

    image: Add a signature fetcher
    asc is used for fetching signatures either from a local filesystem or
    from a remote location.

--- rkt/image/common.go
+++ rkt/image/common.go
@@ -17,6 +17,7 @@ package image
 import (
 	"fmt"
 	"os"
+	"reflect"
 )
 
 // stderr prints messages to standard error. There is no need to add a
@@ -26,4 +27,34 @@ func stderr(format string, a ...interface{}) {
 	prefixedFormat := fmt.Sprintf("rkt: %s", format)
 	out := fmt.Sprintf(prefixedFormat, a...)
 	fmt.Fprintln(os.Stderr, strings.TrimSuffix(out, "\n"))
+}
+
+// isReallyNil makes sure that the passed value is really really
+// nil. So it returns true if value is plain nil or if it is e.g. an
+// interface with non-nil type but nil-value (which normally is
+// different from nil itself).
+func isReallyNil(iface interface{}) bool {
+	// this catches the cases when you pass non-interface nil
+	// directly, like:
+	//
+	// isReallyNil(nil)
+	// var m map[string]string
+	// isReallyNil(m)
+	if iface == nil {
+		return true
+	}
+	// get a reflect value
+	v := reflect.ValueOf(iface)
+	// only channels, functions, interfaces, maps, pointers and
+	// slices are nillable
+	switch v.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		// this catches the cases when you pass some interface
+		// with nil value, like:
+		//
+		// var v io.Closer = func(){var f *os.File; return f}()
+		// isReallyNil(v)
+		return v.IsNil()
+	}
+	return false
 }
